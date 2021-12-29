dac39ab9ef0496920b2d09ac557fc71d5c140d95
impuls1stp23 <92928695+impuls1stp23@users.noreply.github.com>
2021-12-29 10:36:46 +0300

    Update MyMoped.sol

--- MyMoped.sol
+++ MyMoped.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.0;
 
 contract MopedShop {
-    mapping (address => >bool) buyers;
-     uint256public price = 2 ether;
+    mapping (address => bool) buyers;
+     uint256 public price = 2 ether;
     address public owner;
     address public shopAddress;
     bool fullyPaid; // false
@@ -16,19 +16,19 @@ contract MopedShop {
  shopAddress = address(this);
     }
     
-    function getBuyer(address )   publicviewreturns(bool) {
+    function getBuyer(address _addr)   public view returns(bool) {
         require(owner == msg.sender, "You are not an owner!");
         
         return buyers[_addr];
     }
     
-    function addBuyer(address ) public {
+    function addBuyer(address _addr) public {
         require(owner == msg.sender, "You are not an owner!");
         
  buyers[_addr] = true;
     }
     
-    function getBalance()   publicviewreturns(uint) {
+    function getBalance()   public view returns(uint) {
         return shopAddress.balance;
     }
     
