3949a223b3acca57bd07ef3fc04ed6bbeeeb6e84
impuls1stp23 <92928695+impuls1stp23@users.noreply.github.com>
2022-01-26 15:20:39 +0300

    Update ZombieFactory.sol

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -3,7 +3,9 @@
 pragma solidity ^0.4.19;
 
 contract ZombieFactory { //Cоздаем контракт
-
+    
+    event NewZombie(uint zombieId, string name, uint dna); // Объявляем событие для внешнего приложения
+    
     uint dnaDigits = 16;   //ДНК зомби будет определяться номером из 16 цифр
     uint dnaModulus = 10 ** dnaDigits; 
         /** 
@@ -22,13 +24,24 @@ contract ZombieFactory { //Cоздаем контракт
         и он может увеличиваться. Назовем массив zombies
         */  
     function _createZombie(string _name, uint _dna) private {
-            zombies.push(Zombie(_name, _dna));
+            uint id = zombies.push(Zombie(_name, _dna)) - 1;
+            NewZombie(id, _name, _dna);
     } 
         /** функция _createZombie
-        Создем функцию под названием createZombie (создать зомби), которая берет 2 параметра:
+        Создаем функцию под названием createZombie (создать зомби), которая берет 2 параметра:
         _name (имя, строка string) и _dna (ДНК, тип uint)
         с помощью zombies.push добавляем новый объект Zombie (типа структура) в конец массива
         с именем zombies, при этом .push ничего не возвращает
+        */
+        /**
+        Нужно каждый раз сообщать внешнему интерфейсу о создании нового зомби, чтобы приложение могло его отобразить.
+        Задай event (событие) под названием NewZombie (новый зомби). Оно должно сообщать zombieId (uint), 
+        имя name (строку string) и ДНК dna (uint).
+        событие NewZombie запускается после добавления нового солдата в массив zombies.
+        id — идентификатор зомби. array.push() возвращает uint новой длины массива. 
+        Поскольку первый элемент в массиве имеет индекс 0, array.push () - 1 вернет индекс 
+        только что добавленного зомби. Сохрани результат zombies.push () - 1 в uint с названием id,
+        чтобы его можно было использовать в событии NewZombie в следующей строчке.
         */
     function _generateRandomDna(string _str) private view returns (uint) {
         uint rand = uint(keccak256(_str));
@@ -47,12 +60,12 @@ contract ZombieFactory { //Cоздаем контракт
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
-    /** функция createRandomZombie - публичная функция, 
-    которая получает на вход параметр "имя зомби" и использует его, 
-    чтобы создать зомби со случайной ДНК
-    Первая строчка в коде функции должна вызывать _generateRandomDna(сгенерировать случайную ДНК)
-    в _name (имя), и сохранять ее в uint под названием randDna (случайная ДНК).
-    Вторая строчка кода вызывает функцию _createZombie (создать зомби) и сообщает ей _name(имя) 
-    и randDna (случайную ДНК).
-    */ 
+        /** функция createRandomZombie - публичная функция, 
+        которая получает на вход параметр "имя зомби" и использует его, 
+        чтобы создать зомби со случайной ДНК
+        Первая строчка в коде функции должна вызывать _generateRandomDna(сгенерировать случайную ДНК)
+        в _name (имя), и сохранять ее в uint под названием randDna (случайная ДНК).
+        Вторая строчка кода вызывает функцию _createZombie (создать зомби) и сообщает ей _name(имя) 
+        и randDna (случайную ДНК).
+        */ 
 }
